cc9b59de560a11f4dd89de83b2624c5f1384fe63
Jhone Dourado <jhonedourado74@gmail.com>
2023-07-02 21:44:16 -0300

    Adjustments

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 const input = document.querySelector("#input");
-const counterCharactersWithSpace = document.querySelector(".charactersWithSpace");
-const counterCharactersNoSpace = document.querySelector(".charactersNoSpace");
+const counterCharSpace = document.querySelector(".charSpace");
+const counterCharNoSpace = document.querySelector(".charNoSpace");
 const counterWords = document.querySelector(".words");
 
 input.addEventListener("input", () => {
-  let valueCharactersWithSpace = 0;
-  valueCharactersWithSpace = input.value.length;
-  counterCharactersWithSpace.textContent = valueCharactersWithSpace;
+  let countCharSpace = 0;
+  countCharSpace = input.value.length;
+  counterCharSpace.textContent = countCharSpace;
 
-  let valueCharactersNoSpace = 0;
-  valueCharactersNoSpace = input.value.replace(/\s/g,'').length;
-  counterCharactersNoSpace.textContent = valueCharactersNoSpace;
+  let countCharNoSpace = 0;
+  countCharNoSpace = input.value.replace(/\s/g,'').length;
+  counterCharNoSpace.textContent = countCharNoSpace;
 
-  let valueWords = 0;
+  let countWords = 0;
   const words = input.value.trim().split(/\s+/);
-  valueWords = input.value.trim() === "" ? 0 : words.length;
-  counterWords.textContent = valueWords;
+  countWords = input.value.trim() === "" ? 0 : words.length;
+  counterWords.textContent = countWords;
 });
